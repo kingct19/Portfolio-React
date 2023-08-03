b4cd16b7f0804dc67db5f6f26a1e30f493d06fc9
kingct19 <kingchandler19@gmail.com>
2023-08-03 14:42:44 -0500

    About Me Section Complete

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,8 +15,10 @@ const About = () => {
           <Info />
 
           <p className="about__description">
-            Computer Science student currently studying to recieve his masters
-            degree
+            I am Computer Science student currently studying to recieve his
+            masters degree. I have a passion for learning new things and I am
+            always looking for new challenges. I am currently looking for a job
+            in the field of software development.
           </p>
 
           <a download="" href={CV} className="button button--flex">
@@ -27,7 +29,7 @@ const About = () => {
               id="arrow-circle-up"
             >
               <path
-                fill="#6563FF"
+                fill="var(--container-color)"
                 d="M12.71,8.29a1,1,0,0,0-.33-.21,1,1,0,0,0-.76,0,1,1,0,0,0-.33.21l-3,3a1,1,0,0,0,1.42,1.42L11,11.41V15a1,1,0,0,0,2,0V11.41l1.29,1.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42ZM12,2A10,10,0,1,0,22,12,10,10,0,0,0,12,2Zm0,18a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
               ></path>
             </svg>
--- src/components/About/Info.jsx
+++ src/components/About/Info.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 
 const Info = () => {
-  <div className="about__info grid">
-    <div className="about__box"></div>
-  </div>;
+  return (
+    <div className="about__info grid">
+      <div className="about__box">
+        <i class="bx bx-award about__icon"></i>
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">Gradute Student</span>
+      </div>
+
+      <div className="about__box">
+        <i class="bx bxs-graduation about__icon"></i>
+        <h3 className="about__title">Education</h3>
+        <span className="about__subtitle">CS Degree</span>
+      </div>
+
+      <div className="about__box">
+        <i class="bx bx-laptop about__icon"></i>
+        <h3 className="about__title">Git Hub</h3>
+        <span className="about__subtitle">See My Projects</span>
+      </div>
+    </div>
+  );
 };
 
 export default Info;
