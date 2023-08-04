22027a35ace699135641bd9171fe56575b37d9c5
kingct19 <kingchandler19@gmail.com>
2023-08-04 12:37:57 -0500

    Starting Services Section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from "./components/Header/header";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Skills from "./components/Skills/skills";
+import "./components/Services/Services";
+import Services from "./components/Services/Services";
 
 const App = () => {
   return (
@@ -14,6 +16,7 @@ const App = () => {
         <Home />
         <About />
         <Skills />
+        <Services />
       </main>
     </>
   );
